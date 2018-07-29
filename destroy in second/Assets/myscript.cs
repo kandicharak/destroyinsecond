79eaf96781d6096b937fd195c5776c9750adfaff
sumit singh <30618916+kandicharak@users.noreply.github.com>
2018-07-30 00:32:51 +0530

    first day apk

---
+++ destroy in second/Assets/myscript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class myscript : MonoBehaviour {
+    public FixedJoystick movejoystick;
+    public FixedButton jumpbutton;
+    public FixedTouchField touchfield;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var fps = GetComponent<RigidbodyFirstPersonController>();
+        fps.RunAxis = movejoystick.inputVector;
+        fps.JumpAxis = jumpbutton.Pressed;
+        fps.mouseLook.LookAxis = touchfield.TouchDist;
+	}
+}
